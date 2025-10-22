47402830904e5458ee2a5c065d42fcdbc8de9cfd
Achuta Rao <achuta707@gmail.com>
2025-10-22 23:34:01 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  publicDir: 'src/assets/public',
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
+  }
+})
